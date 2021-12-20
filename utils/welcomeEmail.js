ba30c8be6c2b6716cfd4e247a6b07877aa0ccdb9
Ogheneruno <ogefereogheneruno@gmail.com>
2021-12-20 18:59:36 +0100

    welcomeEmail.js

--- utils/welcomeEmail.js
+++ utils/welcomeEmail.js
@@ -2,7 +2,7 @@ const sendEmail = require('../config/mailer');
 
 const welcomeEmail = async (req, username, email, secretToken) => {
     const html = `
-        Welcome to WAAWChat ${username}, we're excited to have you join us.
+        Welcome to WAAW Social ${username}, we're excited to have you join us.
         <br/>
         <br/>
         You can copy and paste the following code <strong>${secretToken}</strong> in the confirmation page
@@ -11,18 +11,18 @@ const welcomeEmail = async (req, username, email, secretToken) => {
         confirmation Link: https://${req.headers.host}/user/confirm-account/${secretToken}
         <br/>
         <br/>
-        WAAWChat will give you the best experience ever!!!!!!!!
+        WAAW Social will give you the best experience ever!!!!!!!!
         <br/>
         <br/>
         Cheers,
         <br/>
-        <strong>WAAWChat Team</strong>
+        <strong>Team WAAW Social</strong>
     `;
 
     await sendEmail (
-        'support@WAAWChat..com',
+        'support@WAAWSocial.com',
         email,
-        'Welcome to WAAWChat',
+        'Welcome to WAAW Social',
         html
     );
 }
